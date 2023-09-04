e9942d4dc587a410b63643be6ef9d4bb296791c2
irtkaal <131509508+irtkaal@users.noreply.github.com>
2023-09-04 23:04:34 +0530

    Update loverconfig.py

--- loverconfig.py
+++ loverconfig.py
@@ -25,7 +25,7 @@ Valid file types are 'jpeg', 'jpg', 'png', 'mp4' and 'gif'.
 
 To generate links in **group chats**, add me to your supergroup and send the command <code>/tl</code> as a reply to a valid media file.
 
-🏠 | [Home](https://t.me/the_chatting)
+🏠 | [Home](https://t.me/EsproSupport)
             """
     await teletips.send_message(message.chat.id, text, disable_web_page_preview=True)
     
